6647a838c0eb3c92858178cf14831c47f5a4f60f
liaosl <975385373@qq.com>
2017-04-22 21:31:39 +0800

    补充学习计划，增加最迫切需要补足的部分

---
+++ src/java/com/wislove/string/StringMethod.java
@@ -0,0 +1,9 @@
+package com.wislove.string;
+
+/**
+ * String 是最常见的一个类
+ * Created by 廖双龙 on 2017/4/22.
+ */
+public class StringMethod {
+
+}
---
+++ src/java/com/wislove/thread/重新学习多线程.md
@@ -0,0 +1,7 @@
+## Java中的String
+
+String是Java中一个经常用到的东西，在《Thinking in Java》中专门有一章是用来写String这个东西的，可见它的重要程度，而在平常的开发中也非常的常见。
+
+
+
+
